252296f3ce9bf393b813d930e6a80d7258367c40
derem1991 <riccardodemagistris@hotmail.it>
2022-03-20 11:58:44 +0100

    Modifica ordine

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('orders:import 1')->everyFiveMinutes(); //import order
     }
 
     /**
